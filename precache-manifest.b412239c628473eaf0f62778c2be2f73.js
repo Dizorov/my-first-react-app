04474bf5338bd4b206a462a9e2ccde7bb3926955
dizorov <dizorov666@gmail.com>
2020-04-12 15:11:14 +0300

    Updates

---
+++ precache-manifest.b412239c628473eaf0f62778c2be2f73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72b752bf242aa398c2e5bda08cd25e7a",
+    "url": "/my-first-react-app/index.html"
+  },
+  {
+    "revision": "b84e821c523f8f9521c4",
+    "url": "/my-first-react-app/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "b1795988b53601a42050",
+    "url": "/my-first-react-app/static/css/main.5d937b94.chunk.css"
+  },
+  {
+    "revision": "b84e821c523f8f9521c4",
+    "url": "/my-first-react-app/static/js/2.0113d603.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/my-first-react-app/static/js/2.0113d603.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1795988b53601a42050",
+    "url": "/my-first-react-app/static/js/main.b2319970.chunk.js"
+  },
+  {
+    "revision": "864477b33ac8ed90cc79",
+    "url": "/my-first-react-app/static/js/runtime-main.f560f3f1.js"
+  }
+]);
